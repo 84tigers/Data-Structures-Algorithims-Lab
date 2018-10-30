ea85ef518ae0c59d1665310610bef6ac3442e230
84tigers <ahyska@gmail.com>
2018-10-30 10:12:25 -0400

    initial

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 "use strict";
 
-$(document).ready(() => {
-    // array to collect input values
-    let bugReq = [];
-    // event listener for button to send values (name, problem) to array
-    $(document).on("click", ".submit", () => {
-        const bugName = $("input.bug-name").val("");
-        const bugProb = $("input.bug-problem").val("");
-        // console.log( $("form.input.bug-name").innerText);
-
-        // send values to end of array
-        bugReq.push({
-            name : bugName,
-            problem : bugProb
-        });    
-    });
-    // console.log(bugReq);
-
-});
-
-
-// newBugReq() function to append <p> with bugReq[0] 
-
-// event listener for completed bugReq -- bugReq.shift() -- call newBugReq() 
 
 
 
